67b68db3b8a4be54c9a084ba78bc4e29bffc42f5
Hao Zhang <hzhangxyz@outlook.com>
2025-07-07 19:38:55 +0800

    Add tests for rule.hh and rule.cc.

--- src/rule.cc
+++ src/rule.cc
@@ -1,21 +1,29 @@
 #include <cstring>
 
+#include <ds/helper.hh>
 #include <ds/rule.hh>
 
 namespace ds {
     term_t* rule_t::conclusion() {
-        return term(get_list_size() - 1);
+        if (get_list_size() > 0) [[likely]] {
+            return term(get_list_size() - 1);
+        } else {
+            return nullptr;
+        }
     }
 
     term_t* rule_t::only_conclusion() {
-        if (premises_count() == 0) {
-            return conclusion();
+        if (get_list_size() == 1) [[likely]] {
+            return term(0);
         } else {
             return nullptr;
         }
     }
 
     term_t* rule_t::premises(length_t index) {
+        if (index < 0 || index >= get_list_size() - 1) [[unlikely]] {
+            return nullptr;
+        }
         return term(index);
     }
 
@@ -28,7 +36,7 @@ namespace ds {
     }
 
     rule_t* rule_t::set_null(std::byte* check_tail) {
-        if (set_list_size(0, check_tail) == nullptr) {
+        if (set_list_size(0, check_tail) == nullptr) [[unlikely]] {
             return nullptr;
         }
         return this;
@@ -47,40 +55,39 @@ namespace ds {
     }
 
     char* rule_t::print(char* buffer, char* check_tail) {
+        if (!valid()) [[unlikely]] {
+            return nullptr;
+        }
+        // 记录premises的最大长度用于分割符的长度
         length_t max_length = 4;
+        // 打印当前term之前的buffer位置
         char* last_buffer = buffer;
         for (length_t index = 0; index < premises_count(); ++index) {
             buffer = premises(index)->print(buffer, check_tail);
-            if (buffer == nullptr) {
+            if (buffer == nullptr) [[unlikely]] {
                 return nullptr;
             }
             length_t this_length = buffer - last_buffer;
             max_length = this_length > max_length ? this_length : max_length;
-            if (check_tail != nullptr) {
-                if (check_tail <= buffer) {
-                    return nullptr;
-                }
+            if (check_till_fail(check_tail, buffer)) [[unlikely]] {
+                return nullptr;
             }
             *(buffer++) = '\n';
             last_buffer = buffer;
         }
-        if (check_tail != nullptr) {
-            if (check_tail <= buffer + max_length + 1) {
-                return nullptr;
-            }
+        if (check_till_fail(check_tail, buffer, max_length)) [[unlikely]] {
+            return nullptr;
         }
         for (length_t length = 0; length < max_length; ++length) {
             *(buffer++) = '-';
         }
         *(buffer++) = '\n';
         buffer = conclusion()->print(buffer, check_tail);
-        if (buffer == nullptr) {
+        if (buffer == nullptr) [[unlikely]] {
             return nullptr;
         }
-        if (check_tail != nullptr) {
-            if (check_tail <= buffer) {
-                return nullptr;
-            }
+        if (check_till_fail(check_tail, buffer)) [[unlikely]] {
+            return nullptr;
         }
         *(buffer++) = '\n';
         return buffer;
@@ -93,8 +100,8 @@ namespace ds {
         term_t* term = reinterpret_cast<term_t*>(this);
         length_t list_size = 0;
         bool last_one = false;
-        while (buffer) {
-            if (*buffer == 0) {
+        while (true) {
+            if (*buffer == '\0') {
                 break;
             }
             if (strchr(" \t\r\n", *buffer)) {
@@ -108,7 +115,7 @@ namespace ds {
                 continue;
             }
             buffer = term->scan(buffer, check_tail);
-            if (buffer == nullptr) {
+            if (buffer == nullptr) [[unlikely]] {
                 return nullptr;
             }
             term = reinterpret_cast<term_t*>(term->tail());
@@ -118,19 +125,14 @@ namespace ds {
             }
         }
         length_t offset = sizeof(length_t) + sizeof(length_t) * list_size;
-        // 需要将rule中的term整体向后移动offset个字节
-        // 需要检查目前的尾巴，也就是term加上offset后是否溢出
-        if (check_tail != nullptr) {
-            if (check_tail < reinterpret_cast<std::byte*>(term) + offset) {
-                return nullptr;
-            }
+        if (check_before_fail(check_tail, term, offset)) [[unlikely]] {
+            return nullptr;
         }
         memmove(
             reinterpret_cast<std::byte*>(this) + offset,
             reinterpret_cast<std::byte*>(this),
             reinterpret_cast<std::byte*>(term) - reinterpret_cast<std::byte*>(this)
         );
-        // 向后移动数据后，不需要检查前面的空间是否足够
         set_list_size(list_size, nullptr);
         for (length_t index = 0; index < list_size; ++index) {
             update_term_size(index);
