e3a61762b78e0e366ef68242b14d2a38c8df1616
Hao Zhang <hzhangxyz@outlook.com>
2025-07-26 00:38:36 +0800

    Add api v1 for ddar engine.

--- examples/ddar.mjs
+++ examples/ddar.mjs
@@ -6,7 +6,8 @@ const rules = await ejs.renderFile("examples/ddar.ejs");
 
 const app = express();
 
-app.use(express.text({ type: "*/*" }));
+app.use(express.text({ type: "application/x-www-form-urlencoded" }));
+app.use(express.json());
 
 app.post("/", (req, res) => {
     const terms = req.body
@@ -34,6 +35,55 @@ app.post("/", (req, res) => {
         }
     }
     res.type("text/plain").send(`${result.join(req.query.sep || "\n")}\n`);
+});
+
+app.post("/api/v1/searchAll", (req, res) => {
+    const rules = req.body.rules;
+    const facts = req.body.facts;
+    const rulesSeperator = new RegExp(req.body.rulesSeperator || ";|\n");
+    const factsSeperator = new RegExp(req.body.factsSeperator || ",|;|\n");
+    const termsSeperator = new RegExp(req.body.termsSeperator || ",|=>");
+    const bufferLimit = req.body.bufferLimit || 1024;
+    const rulesStrings = rules
+        .split(rulesSeperator)
+        .map((x) => x.trim())
+        .filter((x) => x.length > 0);
+    const factsStrings = facts
+        .split(factsSeperator)
+        .map((x) => x.trim())
+        .filter((x) => x.length > 0);
+    const rulesList = rulesStrings.map((x) =>
+        x
+            .split(termsSeperator)
+            .map((x) => x.trim())
+            .filter((x) => x.length > 0)
+            .map((x) => {
+                const [predicate, ...objects] = x
+                    .split(" ")
+                    .map((y) => y.trim())
+                    .filter((y) => y.length > 0);
+                return `(${predicate} ${objects.map((y) => `\`${y}`).join(" ")})`;
+            })
+            .join("\n"),
+    );
+    const factsList = factsStrings.map((x) => `(${x})`);
+    const data = [...rulesList, ...factsList];
+    const result = [];
+    const generator = search(data, bufferLimit, (candidate) => {
+        if (candidate.length() === 0) {
+            const str = candidate.conclusion().toString();
+            const toAppend = str.slice(1, -1);
+            result.push(toAppend);
+        }
+    });
+    while (true) {
+        if (generator.next().done) {
+            break;
+        }
+    }
+    res.json({
+        result,
+    });
 });
 
 app.listen(3000);
