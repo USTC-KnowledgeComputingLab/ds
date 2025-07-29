c8177ff72f807651e0f7c4596fee9e26b0d9f93d
Hao Zhang <hzhangxyz@outlook.com>
2025-07-29 17:53:50 +0800

    Remove most of examples.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,4 @@
-import json from "@rollup/plugin-json";
 import terser from "@rollup/plugin-terser";
-import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import { copy } from "@web/rollup-plugin-copy";
@@ -10,9 +8,6 @@ export default {
         ds: "tsds/ds.mjs",
         tsds: "tsds/tsds.mts",
         example: "examples/main.mjs",
-        search: "examples/search.mjs",
-        engine: "examples/engine.mjs",
-        ddar: "examples/ddar.mjs",
     },
     output: {
         dir: "dist",
@@ -21,9 +16,7 @@ export default {
         entryFileNames: "[name].mjs",
     },
     plugins: [
-        json(),
         terser(),
-        commonjs(),
         typescript(),
         nodeResolve(),
         copy({
