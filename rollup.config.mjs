67cb0fc9e5bde5083177f44cd7d47d54ed1df081
Hao Zhang <hzhangxyz@outlook.com>
2025-11-27 09:03:24 +0800

    Use plugin to merge .d.mts.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,26 +2,39 @@ import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import { copy } from "@web/rollup-plugin-copy";
+import { dts } from "rollup-plugin-dts";
 
-export default {
-    input: {
-        ds: "atsds/ds.mjs",
-        tsds: "atsds/tsds.mts",
-        example: "examples/main.mjs",
+export default [
+    {
+        input: {
+            tsds: "atsds/tsds.mts",
+            example: "examples/main.mjs",
+        },
+        output: {
+            dir: "dist",
+            format: "es",
+            sourcemap: true,
+            entryFileNames: "[name].mjs",
+        },
+        plugins: [
+            terser(),
+            typescript(),
+            nodeResolve(),
+            copy({
+                patterns: ["ds.wasm", "ds.wasm.map"],
+                rootDir: "atsds",
+            }),
+        ],
     },
-    output: {
-        dir: "dist",
-        format: "es",
-        sourcemap: true,
-        entryFileNames: "[name].mjs",
+    {
+        input: {
+            tsds: "atsds/tsds.mts",
+        },
+        output: {
+            dir: "dist",
+            format: "es",
+            entryFileNames: "[name].d.mts",
+        },
+        plugins: [dts()],
     },
-    plugins: [
-        terser(),
-        typescript(),
-        nodeResolve(),
-        copy({
-            patterns: ["ds.wasm", "ds.wasm.map", "ds.d.mts"],
-            rootDir: "atsds",
-        }),
-    ],
-};
+];
